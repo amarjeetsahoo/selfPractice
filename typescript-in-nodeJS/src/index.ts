6c6f4e5a60fd0be9d486dc5522032c429baf08b1
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-05-23 19:18:49 +0530

    init server

--- typescript-in-nodeJS/src/index.ts
+++ typescript-in-nodeJS/src/index.ts
@@ -1,6 +1,10 @@
-import { writeFile } from 'fs';
-import { generate } from 'shortid';
+import express from 'express';
+const app = express();
 
-writeFile;
+app.get('/', (req, res) => {
+    res.send("<h1>Hello I'm TypeScript!</h1>").status(200);
+});
 
-console.log("HelloWorld!");
+app.listen(5000, () => {
+    console.log("HelloWorld!");
+});
