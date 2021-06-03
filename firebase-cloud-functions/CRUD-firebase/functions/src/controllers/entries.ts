308c31108972e1c3518369575d8be3b22c2d98a4
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-06-03 18:14:08 +0530

    CURD-Firebase

---
+++ firebase-cloud-functions/CRUD-firebase/functions/src/controllers/entries.ts
@@ -0,0 +1,97 @@
+import { Response } from "express";
+import { db } from "../config/firebase";
+
+type EntryType = {
+    title: string,
+    text: string
+};
+
+type Request = {
+    body: EntryType,
+    params: {
+        entryId: string
+    }
+};
+
+const addEntry = async (req: Request, res: Response) => {
+    const { title, text } = req.body;
+    try {
+        const entry = db.collection('entries').doc();
+        const entryObject = {
+            id: entry.id,
+            title,
+            text
+        };
+        entry.set(entryObject);
+        res.status(201).send({
+            status: true,
+            message: 'Entry added successfully',
+            data: entryObject
+        });
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+}
+
+const getAllEntries = async (req: Request, res: Response) => {
+    try {
+        const allEntries: EntryType[] = [];
+        const querySnapshot = await db.collection('entries').get()
+        querySnapshot.forEach((doc: any) => allEntries.push(doc.data()));
+        return res.status(200).json(allEntries);
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+
+const updateEntry = async (req: Request, res: Response) => {
+    const { body: { text, title }, params: { entryId } } = req;
+    try {
+        const entry = db.collection('entries').doc(entryId);
+        const currentData = (await entry.get()).data() || {}
+
+        const entryObject = {
+            id: req.params.entryId || currentData.id,
+            title: title || currentData.title,
+            text: text || currentData.text
+        }
+        await entry.set(entryObject);
+        return res.status(201).send({
+            status: true,
+            message: 'Entry updated successfully',
+            data: entryObject
+        });
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+
+const deleteEntry = async (req: Request, res: Response) => {
+    const { entryId } = req.params;
+
+    try {
+        const entry = db.collection('entries').doc(entryId);
+        await entry.delete();
+        return res.status(201).send({
+            status: true,
+            message: 'Entry deleted successfully'
+        });
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+const getEntryById = async (req: Request, res: Response) => {
+    const { entryId } = req.params;
+
+    try {
+        const entry = await db.collection('entries').doc(entryId).get();
+        if (entry.data())
+            return res.status(201).json(entry.data());
+        else
+            return res.status(500).send({status:false});
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+
+export { addEntry, getAllEntries, updateEntry, deleteEntry, getEntryById };
--- firebase-cloud-functions/React-Firebase/client/src/config/config.ts
+++ firebase-cloud-functions/React-Firebase/client/src/config/config.ts
@@ -1,5 +1,12 @@
 const config = {
     firebase: {
+        apiKey: "AIzaSyCl6JNL6uO6_HDEc9pUuZozpiqOBTrkLek",
+        authDomain: "firecast-app-b5ee6.firebaseapp.com",
+        databaseURL: "https://firecast-app-b5ee6-default-rtdb.firebaseio.com",
+        projectId: "firecast-app-b5ee6",
+        storageBucket: "firecast-app-b5ee6.appspot.com",
+        messagingSenderId: "375524306615",
+        appId: "1:375524306615:web:23b505bfe973ecd32fa732"
     }
 }
 
