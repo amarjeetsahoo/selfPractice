610728acc141068632cf55d96a36c19c4ae502f5
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-06-06 17:13:44 +0530

    updated: env

--- firebase-cloud-functions/CRUD-firebase/functions/src/config/firebase.ts
+++ firebase-cloud-functions/CRUD-firebase/functions/src/config/firebase.ts
@@ -1,12 +1,13 @@
 import * as admin from "firebase-admin";
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        privateKey: functions.config().private.key.replace(/\\n/g, '\n'),
-        projectId: functions.config().project.id,
-        clientEmail: functions.config().client.email
-    }),
+    credential:admin.credential.applicationDefault(),
+    // credential: admin.credential.cert({
+    //     privateKey: functions.config().private.key.replace(/\\n/g, '\n'),
+    //     projectId: functions.config().project.id,
+    //     clientEmail: functions.config().client.email
+    // }),
     databaseURL: "https://firecast-app-b5ee6.firebaseio.com"
 });
 
